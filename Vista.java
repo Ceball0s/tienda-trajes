645c71b55f3c5a3cbb00cb57573f9254b1e4251d
Iris <129295205+CamiSaysUwU@users.noreply.github.com>
2023-07-12 09:06:52 -0500

    Add files via upload

---
+++ Vista.java
@@ -0,0 +1,25 @@
+package dulceria_gui;
+import java.util.ArrayList;
+public abstract class Vista {
+
+    // Atributo para el controlador
+    protected Controlador controlador;
+
+    // Método para asignar el controlador a la vista
+    public void setControlador(Controlador controlador) {
+        this.controlador = controlador; // Asignar el controlador a la vista
+    }
+
+    // Método abstracto para hacer visible la ventana
+    public abstract void setVisible(boolean visible);
+
+
+    // Método abstracto para actualizar la lista de Trajes con los datos del modelo
+    public abstract void actualizarLista(ArrayList<Traje> listaTrajes);
+
+    // Método abstracto para mostrar los datos de un Traje en los campos de texto
+    public abstract void mostrarTraje(Traje Traje);
+
+    // Método abstracto para mostrar un mensaje en la consola o en la ventana
+    public abstract void mostrarMensaje(String mensaje);
+}
